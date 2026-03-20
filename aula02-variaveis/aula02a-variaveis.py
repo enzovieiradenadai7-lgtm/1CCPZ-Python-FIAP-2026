84507c1cb94982bca1a8d6a238c39cb79ffe5a8d
enzovieiradenadai7-lgtm <enzovieiradenadai7@gmail.com>
2026-03-20 20:52:15 -0300

    aula2 python

---
+++ aula02-variaveis/aula02a-variaveis.py
@@ -0,0 +1,24 @@
+print ("ola mundo")
+
+print(7+4)
+print("7+4")
+print("7"+"4")
+
+'''
+comentários de 
+múltiplas 
+linhas
+'''
+
+nome= "Alexandre" #string
+idade= 26 #int
+peso= 70.2 #float
+print(nome, idade, peso)
+print(f"Oiii, {nome}!!")
+
+nome=input("digite o seu nome: ")
+idade= int(input("digite sua idade: "))
+nova_idade= idade + 1
+
+print(nome, idade)
+print(nova_idade)
---
+++ aula02-variaveis/exercicios.py
@@ -0,0 +1,12 @@
+print("ola mundo")
+
+nome=input("digite o seu nome: ")
+print(f"Bem Vindo, {nome}!!")
+
+dia= int(input("dia "))
+mês= int(input("mês: "))
+ano= int(input("ano: "))
+
+print(dia, mês, ano)
+
+
